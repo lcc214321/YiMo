87ead588a8cab6a6b6bf0e2bf4faad15479af279
imTayle <imTayle@126.com>
2019-03-28 11:04:25 +0800

    add 增加服务端Xss过滤 基于Jsoup

---
+++ src/main/java/top/yimo/common/util/JsoupUtil.java
@@ -0,0 +1,35 @@
+package top.yimo.common.util;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
+
+public class JsoupUtil {
+	// 使用自带的basicWithImages 白名单
+	private static final Whitelist whitelist = Whitelist.basicWithImages();
+	/** 配置过滤化参数,不对代码进行格式化 */
+	private static final Document.OutputSettings outputSettings = new Document.OutputSettings().prettyPrint(false);
+
+	static {
+		// 富文本编辑时一些样式是使用style来进行实现的
+		// 比如红色字体 style="color:red;"
+		// 所以需要给所有标签添加style属性
+		whitelist.addAttributes(":all", "style");
+	}
+
+	public static String clean(String content) {
+		if (StringUtils.isNotBlank(content)) {
+			content = content.trim();
+		}
+		return Jsoup.clean(content, "", whitelist, outputSettings);
+	}
+
+	public static void main(String[] args) throws IOException {
+		String text = "   <a href=\"http://www.baidu.com/a\" onclick=\"alert(1);\">sss</a><script>alert(0);</script>sss   ";
+		System.out.println(clean(text));
+	}
+
+}
---
+++ src/main/java/top/yimo/common/xss/XssConfig.java
@@ -0,0 +1,50 @@
+package top.yimo.common.xss;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * 
+ * @Author imTayle
+ * @Email imTayle@126.com
+ * @version 1.0
+ * @Time 2019年3月28日 上午10:49:27
+ */
+@Configuration
+public class XssConfig {
+	@Value("${xss.enabled}")
+	private String enabled;
+
+	@Value("${xss.excludes}")
+	private String excludes;
+	@Value("${xss.urlPatterns}")
+	private String urlPatterns;
+
+	/**
+	 * xss过滤拦截器
+	 */
+	@Bean
+	public FilterRegistrationBean<XssFilter> xssFilterRegistrationBean() {
+		FilterRegistrationBean<XssFilter> filterRegistrationBean = new FilterRegistrationBean<XssFilter>();
+		filterRegistrationBean.setFilter(new XssFilter());
+		filterRegistrationBean.setOrder(1);
+		if (StringUtils.isBlank(urlPatterns)) {// 默认过滤所有
+			urlPatterns = "/*";
+		}
+		if (StringUtils.isBlank(enabled)) {// 默认过滤所有
+			enabled = "true";
+		}
+		filterRegistrationBean.addUrlPatterns(urlPatterns);
+		Map<String, String> initParameters = new HashMap<String, String>();
+		initParameters.put("excludes", excludes);
+		initParameters.put("enabled", enabled);
+		filterRegistrationBean.setInitParameters(initParameters);
+		return filterRegistrationBean;
+
+	}
+}
