af09acfb08a90fa5db8767b03157c2da3b3b9a91
Tayle <imTayle@126.com>
2019-04-11 17:38:07 +0800

    update 完善数据字典编辑功能

--- src/main/resources/static/yimo/js/sys/dict/dict.js
+++ src/main/resources/static/yimo/js/sys/dict/dict.js
@@ -44,7 +44,6 @@ function load() {
 							title : '状态',
 							align : 'center',
 							formatter : function(value, row, index) {
-//								console.log(row	);
 								if (value == '0') {
 									return '<span class="label label-danger">禁用</span>';
 								} else if (value == '1') {
